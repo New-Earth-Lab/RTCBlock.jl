afb892ee90147a9c57f148097cf84eb6092347ad
William Thompson <wthompson@uvic.ca>
2023-12-07 16:38:07 -0800

    Support concatenated event messages

--- src/RTCBlock.jl
+++ src/RTCBlock.jl
@@ -66,28 +66,14 @@ function serve(
 
     # Initialize the state machine
     Hsm.transition!(sm, :Top)
-
-    # Keep a queue of commands to apply.
-    # When commands are received, we queue them up.
-    # When a CommitMessage is received, we process all events at once
-    # before returning to processing data.
-    command_message_queue = zeros(UInt8,0)
-    sizehint!(command_message_queue, 2^14)
     
     # After any command style message is received, we publish our current
     # state out to our output status channel.
     status_report_buffer = zeros(UInt8,1024)
-    status_report_msg = CommandMessage(status_report_buffer)
-    status_report_msg.command = "state"
+    status_report_msg = EventMessage(status_report_buffer)
+    status_report_msg.name = "state"
     status_report_msg.header.description = ""
     resize!(status_report_buffer, sizeof(status_report_msg)+32) # allow 32 chars of room for the status value
-
-    # Keep a queue of commands to apply.
-    # When commands are received, we queue them up.
-    # When a CommitMessage is received, we process all events at once
-    # before returning to processing data.
-    command_message_queue = zeros(UInt8,0)
-    sizehint!(command_message_queue, 2^14)
 
 
     # TODO: we could do after on initialize in the RT loop.
@@ -112,6 +98,7 @@ function serve(
         # for loop unrolls.
 
         while true
+            yield() # temp debug
             # Process any data
             for sub in subs_data
                 bytesread, data = Aeron.poll(sub)
@@ -129,44 +116,32 @@ function serve(
             # TODO: we are going to simplify the commit logic 
 
             # A command / event to process (after the next commit is received)
-            if msg_name == :CommandMessage
-                cmd = CommandMessage(data.buffer)
-                # @info "Command received, queueing" cmd.command
-                # Queue received commands back to back in a buffer.
-                startpos = sizeof(command_message_queue)+1
-                resize!(command_message_queue, sizeof(command_message_queue)+sizeof(cmd))
-                command_message_queue[startpos:end] .= view(data.buffer, 1:sizeof(cmd))
-            
-            # Commit received, process all events
-            elseif msg_name == :CommitMessage
-                # @info "Commit Received"
-                commit_msg = CommitMessage(data.buffer)
-                startpos = 1
-                prevstate = Hsm.current(sm)
-                while startpos < length(command_message_queue)
-                    # @info "processing command"
-                    cmd_data = @view command_message_queue[startpos:end]
-                    cmd = CommandMessage(cmd_data)
-                    startpos += sizeof(cmd) # Bump start position for next iteration
-                    # Check that the correlation Id matches the commit (otherwise discard)
-                    if cmd.header.correlationId != commit_msg.header.correlationId
-                        continue
-                    end
+            if msg_name == :EventMessage
+
+                # We can have multiple event messages concatenated together.
+                # In this case, we apply each sequentually in one go. 
+                # This allows changing multiple parameters "atomically" between
+                # loop updates.
+                last_ind = 0
+                while last_ind < length(data.buffer) # TODO: don't fail if there are a few bytes left over
+                    data_span = @view data.buffer[last_ind+1:end]
+                    event = EventMessage(data_span, initialize=false)
+                    event_data = view(data_span, 1:sizeof(event))
                     
-                    cmd = CommandMessage(cmd_data)
-                    event_name = Symbol(cmd.command)
-                    handled = Hsm.dispatch!(sm, event_name, cmd_data)
-                    
+                    # Dispatch event
+                    event_name = Symbol(event.name)
+                    prevstate = Hsm.current(sm)
+                    handled = Hsm.dispatch!(sm, event_name, event_data)
                     afterstate = Hsm.current(sm)
+
                     # TODO: we're still sending acknowledgements even when an event is not handled. Can we use a return value of dispatch!?
                     # Check we haven't fallen into an error state or error sub-state
                     if handled && !Hsm.ischildof(sm, afterstate, :Error)
                         # Republish this message to our status channel so that senders
                         # can know we have received and dealt with their command
                         # This is a form of *acknowledgement*
-                        Aeron.put!(pub_status, cmd_data)
+                        Aeron.put!(pub_status, event_data)
                     end
-
 
                     # If the state has changed, publish our current state. This is so listeners know
                     # what we're doing without having to keep look at our history
@@ -178,9 +153,16 @@ function serve(
                         Aeron.put!(pub_status, status_report_buffer)
                         # Note, we didn't shrink the status_report_buffer so it might have some hanging bytes
                     end
+                    @show afterstate
 
+                    last_ind += sizeof(event)
                 end
-                resize!(command_message_queue, 0)
+            elseif msg_name == :StatusRequestMessage
+                msg = StatusRequestMessage(data.buffer)
+                # get current state
+                state_named_tuple = statevariables(sm)
+                # TODO: loop through state and publish each message.
+                # correlationId should match the request
             else
                 @warn "unhandled message received" maxlog=1
             end
